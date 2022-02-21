e3edcd1366fb11f481205b036dff246b97febf38
AsadullaK <rahmanmdmotiar85@gmail.com>
2022-02-21 15:30:38 +0600

    data type

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log('Hello World!');
+// fast code
+document.write("hello world")
+document.write("<b><i> hello world</i></b>")
+//hear is a single line comment
+/*hear is a multiple 
+
+comment*/
+//JavaScript variable var, let , const
+var x = " <br/>variable var";
+var x = " second var";
+var y;
+y = "<br/>it is Y";
+document.write(x);
+document.write(y);
+//let
+let letx ="<br/>variable let";
+//let letx;
+letx =" second let";
+document.write(letx);
+// const variable
+const constx = "<br/>const hello";
+/*const constx;
+constx = "hello everyone";*/
+//const constx =" multiple const"
+document.write(constx);
+//JavaScript Data types
+// string
+document.write("<br>it's a string");
+var number = 34;
+document.write(number);
+var booliean = true;
+document.write(booliean);
+//array
+var array = ["html" , "css" , "js"];
+document.write("<br>");
+document.write(array);
+document.write("<br>");
+//object
+var object = { fast:"asadulla" , last:"khatami" }
+document.write(object);
+document.write("<br>");
+// null & undefined
+var n = null;
+document.write(n);
+document.write("<br>");
+var u = undefined;
+document.write(u);
+// JavaScript oparators
+
+
+
+
+
+
+
+
